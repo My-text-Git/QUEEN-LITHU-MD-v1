484d65075568b9b7eb150d284d0c178085103362
My-text-Git <webmy5404@gmail.com>
2024-12-08 03:22:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "please enter your session code 😊😊",
+SESSION_ID: process.env.SESSION_ID || "GVAWyKLZ#JVeJlRsEwVQFSsuxB2u0_udWGV6O7gnDkZKDOHgv4Ng",
 MONGODB: process.env.MONGODB || "mongodb+srv://lithumhelitha2008:7rDwXUVeTnIfmnjl@cluster0.1d0bq.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_5278.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
